3d930a4d404a3e0a946358b1cb322db0362b9674
2002prb <2002prb@gmail.com>
2024-04-15 18:03:37 -0700

    ASCII ART!! + package.json and package-lock.json

--- asciiArt.js
+++ asciiArt.js
@@ -1,6 +1,14 @@
 const { argv } = require('node:process');
+const figlet = require('figlet');
 
 // print process.argv
-for (let i = 0; i < process.argv.length; i++) {
-  console.log(`${i} -> ${process.argv[i]}`);
+for (let i = 2; i < process.argv.length; i++) {
+  figlet.text(process.argv[i], (err, data) => {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+    });
 }
